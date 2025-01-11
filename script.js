4389e988986f7bdb84cf84142f7de6c7d4344800
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-11 12:47:11 +0300

    Добавлен лайтбокс картики, который вызывается при клике

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const mainImg = document.getElementById("main-img");
-
+const lightbox = document.querySelector(".lightbox");
 const thumbnails = document.querySelectorAll(".thumbnail");
+const lightboxImage = document.getElementById("lightbox-image");
 
+// Меняем главное изображение при клике на миниатюру
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener("click", () => {
-    mainImg.src = thumbnail.src;
+    mainImg.src = thumbnail.src; // Меняем главное изображение
   });
+});
+
+// Открываем Lightbox при клике на главное изображение
+mainImg.addEventListener("click", () => {
+  lightboxImage.src = mainImg.src; // Устанавливаем картинку в Lightbox
+  lightbox.classList.remove("hidden"); // Показываем Lightbox
+});
+
+// Закрываем Lightbox при клике на любое место в нем
+lightbox.addEventListener("click", () => {
+  lightbox.classList.add("hidden"); // Прячем Lightbox
 });
